73e6c7458261854564eb72db775646d81e6a543b
sowrav10 <sourav.raj778@gmail.com>
2025-07-08 03:10:07 +0600

    2nd page

--- src/cinxo/CinXo.java
+++ src/cinxo/CinXo.java
@@ -4,11 +4,13 @@
  */
 package cinxo;
 
+import javafx.scene.input.MouseEvent;  
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -16,11 +18,31 @@ import javafx.stage.Stage;
  */
 public class CinXo extends Application {
     
+    private double x = 0;
+    private double y = 0;
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
+        
+        
         Scene scene = new Scene(root);
+        
+        root.setOnMousePressed ((MouseEvent event)->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+    
+    });
+        
+        root.setOnMouseDragged((MouseEvent event) -> {
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+        });
+                
+                
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
         
         stage.setScene(scene);
         stage.show();
--- src/cinxo/FXMLDocumentController.java
+++ src/cinxo/FXMLDocumentController.java
@@ -9,7 +9,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -18,6 +23,32 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     private Label label;
+    @FXML
+    private AnchorPane signin_form;
+    @FXML
+    private Button signup_close;
+    @FXML
+    private Button signup_minimize;
+    @FXML
+    private TextField signin_username;
+    @FXML
+    private PasswordField signin_password;
+    @FXML
+    private Button signin_loginbtn;
+    @FXML
+    private Hyperlink signin_create_ac;
+    @FXML
+    private AnchorPane signup_form;
+    @FXML
+    private TextField signup_username;
+    @FXML
+    private PasswordField signup_password;
+    @FXML
+    private Button signup_singupbtn;
+    @FXML
+    private Hyperlink signup_alreadyhaveac;
+    @FXML
+    private TextField signup_email;
     
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
