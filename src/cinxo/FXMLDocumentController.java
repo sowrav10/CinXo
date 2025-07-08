8c900f979a952da043afd4e5c0c385fb9e7a1a32
sowrav10 <sourav.raj778@gmail.com>
2025-07-08 11:19:53 +0600

    button function completed

--- src/cinxo/FXMLDocumentController.java
+++ src/cinxo/FXMLDocumentController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -29,7 +30,7 @@ public class FXMLDocumentController implements Initializable {
     private Button signup_close;
     @FXML
     private Button signup_minimize;
-    @FXML
+    @FXML 
     private TextField signin_username;
     @FXML
     private PasswordField signin_password;
@@ -50,11 +51,44 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextField signup_email;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    public void switchForm(ActionEvent event){
+    if(event.getSource()== signin_create_ac){
+    
+        
+        signin_form.setVisible(false);
+    signup_form.setVisible(true);
+    
+    } else if (event.getSource()== signup_alreadyhaveac ){
+        signin_form.setVisible(true);
+        signup_form.setVisible(false);
+        
+        
+       }
     }
     
+    public void signin_close(){
+    System.exit(0);
+    
+    }
+    
+    public void signin_minimize(){
+        Stage stage= (Stage)signin_form.getScene().getWindow();
+    
+    stage.setIconified(true);
+    }
+    
+    public void signup_close(){
+    System.exit(0);
+    
+    }
+    
+    public void signup_minimize(){
+        Stage stage= (Stage)signin_form.getScene().getWindow();
+    
+    stage.setIconified(true);
+    }
+    
+  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
